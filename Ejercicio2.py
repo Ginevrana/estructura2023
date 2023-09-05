2a127535e4a0c80e821f6b7d7d0fe27d4f4f2e48
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-09-05 11:51:24 -0300

    Se realiza punto 1 de ejercicio 2

---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+# Ejercicio 1
+nro=int(input("Por favor ingrese un número impar: "))
+
+while nro%2==0:
+    nro=int(input("El número ingresado no es impar, por favor ingrese un número impar: "))
+print("El número ingresado es impar")
