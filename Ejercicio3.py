04e81698ff6720e2aeec7d2491a2ec195fbc1b35
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-09-05 13:31:41 -0300

    Se realiza punto 2 de ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,6 +1,15 @@
 # Punto 1
-def area_rectangulo(b,a):
-    resultado= b*a
-    print(f"El área del rectángulo es {resultado}")
+# def area_rectangulo(b,a):
+#     resultado= b*a
+#     print(f"El área del rectángulo es {resultado}")
 
-area_rectangulo(5,3)
+# area_rectangulo(5,3)
+
+# Punto 2
+pi=3.14159
+
+def area_circulo(r):
+    resultado=pi*(r**2)
+    print(f"El área del círculo es {resultado}")
+
+area_circulo(5)
