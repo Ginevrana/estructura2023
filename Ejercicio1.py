6afd2b80e4cb1825ccd8591338b80e8abf32c580
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-09-05 11:41:35 -0300

    Se realiza punto 3 de ejercitación

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -9,3 +9,9 @@ x= "¡Hola "
 z= "Mundo!"
 concatenacion=x+z
 print(concatenacion)
+
+# Punto 3
+nombre=input("Por favor, ingrese su nombre: ")
+dni=input("Por favor, ingrese su DNI: ")
+
+print(f"El nombre del usuario es {nombre} y su DNI es {dni}")
