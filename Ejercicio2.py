c39c6b83a61f5b4265e4c47ab08158bb1d670095
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-09-05 12:25:35 -0300

    Se realiza punto 3 de ejercitación 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -6,19 +6,31 @@
 # print("El número ingresado es impar")
 
 # Ejercicio 2
-nro1=int(input("Por favor, ingrese un número: "))
-nro2=int(input("Por favor, ingrese otro número: "))
-opcion=int(input("Por favor seleccione qué operación realizar con los números ingresados: \n1. Sumar \n2. Restar \n3. Multiplicar \nOpcion elegida: "))
+# nro1=int(input("Por favor, ingrese un número: "))
+# nro2=int(input("Por favor, ingrese otro número: "))
+# opcion=int(input("Por favor seleccione qué operación realizar con los números ingresados: \n1. Sumar \n2. Restar \n3. Multiplicar \nOpcion elegida: "))
 
-if opcion == 1:
-    resultado=nro1+nro2
-    print(f"El resultado de la suma es {resultado}")
-elif opcion == 2:
-    resultado=nro1-nro2
-    print(f"El resultado de la resta es {resultado}")
-elif opcion == 3:
-    resultado=nro1*nro2
-    print(f"El resultado de la multiplicación es {resultado}")
+# if opcion == 1:
+#     resultado=nro1+nro2
+#     print(f"El resultado de la suma es {resultado}")
+# elif opcion == 2:
+#     resultado=nro1-nro2
+#     print(f"El resultado de la resta es {resultado}")
+# elif opcion == 3:
+#     resultado=nro1*nro2
+#     print(f"El resultado de la multiplicación es {resultado}")
+# else:
+#     opcion=int(input("Opción inexistente. \nPor favor seleccione qué operación realizar con los números ingresados: \n1. Sumar \n2. Restar \n3. Multiplicar \nOpcion elegida: "))
+    
+# Punto 3
+mail=input("Por favor, ingrese su email: ")
+
+for caracter in mail:
+    if caracter != "@":
+        continue
 else:
-    opcion=int(input("Opción inexistente. \nPor favor seleccione qué operación realizar con los números ingresados: \n1. Sumar \n2. Restar \n3. Multiplicar \nOpcion elegida: "))
+    mail=input("El mail ingresado es incorrecto. \nPor favor, ingrese su email: ")
+
+print("El mail ingresado es correcto.")
+
     
